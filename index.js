54079e87a3c817644f5ac9c33709a6ae9c580c64
Guy Vider <guy@guyvider.com>
2022-05-03 09:33:40 -0700

    1. use contract values 2. get unminted 3. optimize calls based on prev success

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 import { promises as fs } from 'fs';
 import fetch from 'node-fetch';
 import Web3 from 'web3';
+import abi from './abi.json' assert {type: "json"};
 import {} from 'dotenv/config'
 
-const collection = process.env.COLLECTION;
-const baseURI = process.env.BASE_URI;
+var tokenInfo;
 
-const getTotalSupply = async () => {
+const getTokenInfo = async () => {
   const rpcURL = 'https://mainnet.infura.io/v3/' + process.env.INFURA_KEY;
   const web3 = new Web3(rpcURL);
-  const abi = [{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
   const contract = new web3.eth.Contract(abi, process.env.CONTRACT);
+  let name, symbol, totalSupply, maxSupply, baseURI;
   try {
-    return await contract.methods.totalSupply().call();
+    name = await contract.methods.name().call();
+    symbol = await contract.methods.symbol().call();
+    totalSupply = await contract.methods.totalSupply().call();
+    //turns out not every contract uses this - will need to get baseURI from env :(
+    try {
+      baseURI = await contract.methods.baseURI().call();
+    }
+    catch(err) {
+      baseURI = process.env.BASE_URI;
+    }
+    //if the contract does not provide max supply, take from env
+    try {
+      maxSupply = await contract.methods.maxSupply().call();
+    }
+    catch(err) {
+      maxSupply = process.env.MAX_SUPPLY;
+    }
   }
   catch(err) {
     console.error(err);
-    return 0;
+  }
+  finally {
+    return {name, symbol, totalSupply, maxSupply, baseURI};
   }
 };
 
+const fileExists = async path => !!(await fs.stat(path).catch(e => false));
+
+const createResultsFolder = async symbol => {
+  const path = `./results/${symbol}`;
+  if(!await fileExists(path)) {
+    await fs.mkdir(path);
+  }
+  return path;
+};
+
+
 const getJSON = async n => {
-  const uri = `${baseURI}${n}.json`;
+  const uri = `${tokenInfo.baseURI}${n}.json`;
+  const path = `${tokenInfo.resultsFolder}/${n}.json`;
   try {
+    if(await fileExists(path)) return NaN; //we already got that json
     const result = await (await fetch(uri)).text();
-    await fs.writeFile(`./results/${collection}/${n}.json`, result, 'utf-8');
+    await fs.writeFile(path, result, 'utf-8');
     return NaN;
   }
   catch(err) {
@@ -41,15 +72,15 @@ const recursiveGet = async tokens => {
 };
 
 const main = async () => {
-  const startToken = 4001;
-  const endToken = 4100;
-  const lastMinted = await getTotalSupply();
-  console.log('last token minted:', lastMinted);
+  tokenInfo = await getTokenInfo();
+  console.log(tokenInfo);
+  const startToken = Number(tokenInfo.totalSupply) + 1; //start at the first unminted token
+  const endToken = Number(tokenInfo.maxSupply);
+  tokenInfo.resultsFolder = await createResultsFolder(tokenInfo.symbol);
   //create array containing numbers from start to end
   // const tokens = [...Array(endToken - startToken).keys()].map(x => x + startToken);
   //a nicer way
   const tokens = Array.from({length: endToken - startToken + 1}, (_, i) => i + startToken);
-  // const tokens = [];
   await recursiveGet(tokens);
 };
 
